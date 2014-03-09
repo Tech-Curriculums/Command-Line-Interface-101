13d56592f47c809ef6fe909091c87a85d5632f6a
Gregory S. Kielian <gregory.kielian@gmail.com>
2014-03-08 17:23:20 -0800

    Create FasT_Editing.sh

---
+++ CLI-101/VIM-Tutorials/FasT_Editing.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#This file is broken
+
+#This game tests how fast you can cure random errors in a file
+
+urname=$1 #the user name for the victory bell
+
+echo "User $urname, has completed the challenge at `date`" | cowsay -f dragon | wall
